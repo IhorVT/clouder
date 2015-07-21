abc23adebfd1d5b1b7ddb516decebc5cae6148de
IhorVT <syberio@ukr.net>
2015-07-21 12:59:21 +0300

    almost there
    almost

--- js/long-term.js
+++ js/long-term.js
@@ -21,16 +21,58 @@ function loadDataLong(flag) {
     var cache = JSON.parse(localStorage.defaultWeather);
     console.log(cache);
     console.log(cache.data.city.name);
+    rollUP();
     if (flag === 'madeNew') {
         defPosLong('q=' + cache.data.city.name)
     }
     document.getElementById('cityTitle').innerHTML = cache.data.city.name + ', ' + cache.data.city.country;
+
+    $.each(cache.data.list.slice(0,7), function(){
+        rollDown(
+            dateHandL(this.dt,'date'),
+            dateHandL(this.dt,'day'),
+            this.weather[0].icon,
+            Math.round(this.temp.day) + '&deg;C',
+            this.rain,
+            this.humidity,
+            this.pressure,
+            windDirection(this.deg),
+            this.weather[0].description
+        );
+    });
+}
+
+function rollDown(dateD,day, icon, temp ,precip,hmd,prs,wind,alt){
+    var prc;
+    if(typeof precip==='undefined'){
+        prc='0 mm';
+    } else{
+        prc=precip+' mm';
+    }
+
+    var markup = '<tr>'+
+        '<td>' + dateD + '</td>' + '<td>' + day + '</td>'+ '<td>' + '<img width="73px" height="54px" src="../img/'+ icon +'.png" alt='+alt+'/>'+'<td>' + temp + '</td>' +
+        '<td>' + prc + '</td>'+'<td>' + hmd + '</td>'+'<td>' + prs + '</td>'+'<td>' + wind + '</td>'
+        + '</tr>';
+    sevenDays.insertRow(-1).innerHTML = markup; // Додаємо рядок до таблиці
 }
 
 
-
-
-
+function rollUP(){
+    sevenDays.innerHTML ='';
+    var tmp='<caption>Forecast for seven days</caption>'+
+    '<tr>'+
+    '<th>Date</th>'+
+    '<th>Day</th>'+
+    '<th>Conditions</th>'+
+    '<th>Temperature</th>'+
+    '<th>Precipitation</th>'+
+    '<th>Humidity</th>'+
+    '<th>Pressure</th>'
+    '<th>Wind</th>'+
+    '</tr>';
+    sevenDays.innerHTML=tmp;
+}
 
 
 /* Checking if weather forecast has been launched on this browser */
@@ -41,9 +83,18 @@ if(typeof localStorage.defaultWeather==='undefined'){
 }
 
 
-/*BUTTONS AND other stuff*/
+/*BUTTONS AND OTHER stuff*/
 
-/*Function that activates navigator*/
+/*input form handler*/
+$('#locationForm').submit(sLoc);
+
+function sLoc(e) {
+    e.preventDefault();
+    var pos='q='+($("input:first").val());
+    defPosLong(pos);
+}
+
+/*navigation button*/
 function currentPosition(){
     if (navigator.geolocation) {
         console.log('succes');
@@ -60,4 +111,29 @@ function locFinded(position){
     var pstn='lat='+position.coords.latitude+'&lon='+position.coords.longitude;
     console.log(pstn);
     defPosLong(pstn);
+}
+
+
+/* Date handler for Today forecast with shifting feature in case if in JSON response first date stamp mismatch with local date */
+function dateHandL(dUnix,paramRq){
+    var dTime= new Date(dUnix*1000),
+        month=dTime.getMonth(),
+        dDate=dTime.getDate(),
+        day=dTime.getDay();
+    var monthArray=["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"];
+    var dayArray=['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+
+    switch (paramRq){
+        case 'date':
+            return monthArray[month]+' '+dDate;
+        case 'day':
+            return dayArray[day];
+    }
+
+}
+
+function windDirection(degree){
+    var val=parseInt(degree/22.5 +0.5);//adding 0,5 for proper truncation
+    var dirArray=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
+    return dirArray[(val%16)];//angle changes at every 22.5 degrees, 360/22.5=16, there is 16 directions
 }
